c5fa4287536b511ba5d414a94680a57a2999d652
takayukioda <takayuki@odashi.me>
2016-02-03 23:02:23 +0900

    Implement `GET /me`

--- circlecli.js
+++ circlecli.js
@@ -3,8 +3,17 @@
 
 var
   argv = require('yargs').argv,
-  sub  = argv._[0];
+  sub  = argv._[0],
+  CircleCI = require('./lib/circleci');
+
+var
+  ci = new CircleCI(process.env.CIRCLECI_TOKEN);
 
 if (sub === "me") {
-
+  ci.me()
+    .then((respdata) => {
+      console.log(respdata);
+    }).catch((error) => {
+      console.log(error);
+    });
 }
